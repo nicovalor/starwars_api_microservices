7b08a260bbbe3dae9e525393e1520c509dfad65f
Nicolás Valor <nicoo.valor@gmail.com>
2023-09-23 15:51:44 -0300

    Add static methods to filmSchema

--- database/src/database/schemas/filmSchema.js
+++ database/src/database/schemas/filmSchema.js
@@ -11,4 +11,19 @@ const filmSchema = new Schema({
   planets: [{ type: String, ref: "Planet" }],
 });
 
+filmSchema.statics.list = async function () {
+  return await this.find()
+    .populate("characters", ["_id", "name"])
+    .populate("planets", ["_id", "name"]);
+};
+
+filmSchema.statics.get = async function (_id) {
+  return await this.findById(_id)
+    .populate("characters", ["_id", "name"])
+    .populate("planets", ["_id", "name"]);
+};
+
+filmSchema.statics.insert = async function (film) {
+  return await this.create(film);
+};
 module.exports = filmSchema;
