4931550d6b1796271725d562db5caee7a19faad0
Nicolás Valor <nicoo.valor@gmail.com>
2023-09-23 15:55:42 -0300

    Add static methos to planet schema

--- database/src/database/schemas/planetSchema.js
+++ database/src/database/schemas/planetSchema.js
@@ -14,4 +14,20 @@ const planetSchema = new Schema({
   films: [{ type: String, ref: "Film" }],
 });
 
+planetSchema.statics.list = async function () {
+  return await this.find()
+    .populate("residents", ["_id", "name"])
+    .populate("films", ["_id", "title"]);
+};
+
+planetSchema.statics.get = async function (_id) {
+  return this.findById(_id)
+    .populate("residents", ["_id", "name"])
+    .populate("films", ["_id", "title"]);
+};
+
+planetSchema.statics.insert = async function (planet) {
+  return await this.create(planet);
+};
+
 module.exports = planetSchema;
