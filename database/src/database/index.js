d6500c9d96945a88cd65d77c411fff5d0ef97541
Nicolás Valor <nicoo.valor@gmail.com>
2023-09-23 14:14:22 -0300

    Define models

--- database/src/database/index.js
+++ database/src/database/index.js
@@ -4,5 +4,13 @@ const { MONGO_URI } = require("../config/envs");
 const conn = mongoose.createConnection(MONGO_URI);
 
 const Character = conn.model("Character", require("./schemas/characterSchema"));
+const Film = conn.model("Film", require("./schemas/filmSchema"));
+const Planet = conn.model("Planet", require("./schemas/planetSchema"));
 
-Character.find().then((res) => console.log(res));
+// Character.find().then((res) => console.log(res));
+
+module.exports = {
+  Character,
+  Film,
+  Planet,
+};
