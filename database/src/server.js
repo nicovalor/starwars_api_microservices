ad0393b8bb806e206de0b113b6a243ceb296b580
Nicolás Valor <nicoo.valor@gmail.com>
2023-09-23 19:34:00 -0300

    Add router as middleware

--- database/src/server.js
+++ database/src/server.js
@@ -1,10 +1,13 @@
 const morgan = require("morgan");
 
+const router = require("./database/routes");
+
 const express = require("express");
 
 const server = express();
 
 server.use(express.json());
 server.use(morgan("dev"));
+server.use(router);
 
 module.exports = server;
